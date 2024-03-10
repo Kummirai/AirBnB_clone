4708f39b5f4ba0766ab2aebf3e78bb4f8cecccbb
Kummirai <ajaxmilton@hotmail.com>
2024-03-10 12:44:55 -0700

    Fix base modl code

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,22 @@
-from . import base_model
-from . import user
-from .engine.file_storage import FileStorage
 
-all = [base_model.BaseModel, user.User]
+#!/usr/bin/python3
+""" init file """
+from models.base_model import BaseModel
+from models.engine import file_storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-storage = FileStorage()
-storage.reload()
+classes = {'BaseModel': BaseModel,
+           'User': User,
+           'State': State,
+           'City': City,
+           'Amenity': Amenity,
+           'Place': Place,
+           'Review': Review}
+
+storage = file_storage.FileStorage()
+storage.reload(
