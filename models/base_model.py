0e98b56eb15e87dd3ca39907fb5370df0b9347d2
Kummirai <ajaxmilton@hotmail.com>
2024-03-10 12:43:53 -0700

    .

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,43 @@
-#!/usr/bn/python3
-
-"""
-Defines all common attributes/methods for other classes
-
-"""
-
-import uuid
+#!/usr/bin/python3
 from datetime import datetime
+import models
+from uuid import uuid4
+"""
+module
+"""
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
 
-    def save(self):
-        self.updated_at = datetime.now()
-
-    def to_dict(self):
-        data = self.__dict__.copy()
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
-        data['__class__'] = self.__class__.__name__
-        return data
+class BaseModel():
+    """ Parent of all other classes """
+    def __init__(self, *args, **kwargs):
+        """ Creates attributes """
+        format = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    v = datetime.strptime(v, format)
+                if k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        """ Returns a formatted string of class attributes """
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                      self.__dict__))
+
+    def save(self):
+        """ Updates updated_at value to current time """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """ Creates a dictionary of class attributes """
+        d = self.__dict__.copy()
+        d['__class__'] = self.__class__.__name__
+        d['created_at'] = self.created_at.isoformat()
+        d['updated_at'] = self.updated_at.isoformat()
+        return d
